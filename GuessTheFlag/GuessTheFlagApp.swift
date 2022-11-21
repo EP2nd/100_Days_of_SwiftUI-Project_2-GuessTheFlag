74b06654a32e79bdaf3c55eb402e0ec9ebbec46f
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-11-21 13:33:07 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Edwin Przeźwiecki Jr. on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
